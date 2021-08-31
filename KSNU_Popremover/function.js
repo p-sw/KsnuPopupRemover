e94bffadd5716fe45942b90f630e15cc23d65a5b
Antibiotics <abx@abx.pe.kr>
2021-08-31 17:39:05 +0900

    v1.1
    UI 수정

--- KSNU_Popremover/function.js
+++ KSNU_Popremover/function.js
@@ -2,8 +2,8 @@ setTimeout(function() {
 	var ks_btn = document.querySelectorAll('.popupLayer > .footer > ul > li > a');
 	ks_btn[0].click();
 	//fn_closePop();
-}, 500);
+}, 100);
 /* 
- * 500은 매크로의 반복클릭 빈도를 0.5초로 세팅해둔 것입니다.
+ * 100은 매크로의 반복클릭 빈도를 0.1초로 세팅해둔 것입니다.
  * 이 숫자를 수정하여 반복클릭 빈도를 조정할 수 있습니다.
  */
