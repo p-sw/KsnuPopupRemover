cd206252d44f5e738b03fe1b0c77f98774cecd6b
Shinwoo PARK <devpysweb@gmail.com>
2024-05-04 22:01:37 +0900

    fix: change korean to english to avoid atob error

--- loaderscript.js
+++ loaderscript.js
@@ -1,22 +1,22 @@
 (
   () => {
-    console.log("프로그램 로딩중...");
+    console.log("Loading program...");
     fetch("https://raw.githubusercontent.com/p-sw/KsnuPopupRemover/main/clicker.js").then((res) => {
       if (!res.ok) {
-        throw Error(`프로그램의 코드를 로딩하는 중에 오류가 발생했습니다. (${res.status} ${res.statusText})`)
+        throw Error(`Error occurred while loading program code. (${res.status} ${res.statusText})`)
       }
 
       return res.text();
     })
       .then(
         (code) => {
-          console.log("프로그램 로딩 완료, 페이지에 주입..")
-          eval(code);
+          console.log("Program loaded, injecting to page..")
+          (0, eval)(code);
         }
       )
       .then(
         (code) => {
-          console.log("주입 완료, 프로그램 실행...");
+          console.log("Injection complete, running program...");
           loadPopupRemover();
         }
       )
