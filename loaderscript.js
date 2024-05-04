8a3244cde92546cb6be39d8e3f9995ee0c086209
Shinwoo PARK <devpysweb@gmail.com>
2024-05-04 23:16:51 +0900

    refactor: static instead of fetch

--- loaderscript.js
+++ loaderscript.js
@@ -1,38 +1,39 @@
-var __ksnupopupremover;
-var stopPopupRemover;
-(
-  () => {
-    console.log("Loading program...");
-    fetch("https://raw.githubusercontent.com/p-sw/KsnuPopupRemover/main/clicker.js", {cache:'no-store'}).then((res) => {
-      if (!res.ok) {
-        throw Error("Error occurred while loading program code. ("+res.status+" "+res.statusText+")")
-      }
-      const code = res.text();
-      console.log("Program loaded");
-      return code;
-    })
-      .then(
-        (code) => {
-          console.log("Injecting to page..")
-          eval(code);
-          console.log("Injection completed");
-          console.log("Running program...");
-          __ksnupopupremover = new PopupRemover();
-          __ksnupopupremover.start();
-          console.log("Started program");
-          stopPopupRemover = () => {
-            if (!__ksnupopupremover || !__ksnupopupremover || !__ksnupopupremover.interval) {
-              console.log("Popup remover is not started");
-              return;
-            }
-            __ksnupopupremover.stop()
-          }
-        }
-      )
-      .catch(
-        (err) => {
-          throw err;
-        }
-      );
+class R {
+	constructor() {}
+	
+	start() {
+		this.interval = setInterval(function() {
+			let lecture_iframe = document.getElementById("contentsCheckForm");
+		
+			console.log("lecture_iframe:", lecture_iframe);
+			if (!lecture_iframe)
+				return;
+		
+			let lecture_btn = lecture_iframe.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
+			console.log("lecture_btn: ", lecture_btn);
+		
+			if (!lecture_btn || typeof lecture_btn.click !== "function")
+				return;
+			
+			lecture_btn.click();
+		}, 500);
+	}
+
+	stop() {
+		if (this.interval) {
+			clearInterval(this.interval);
+			this.interval = null;
+		}
+	}
+}
+
+p = new R();
+p.start();
+
+function stopPopupRemover() {
+  if (!p || !p.interval) {
+    console.log("Not started yet");
+    return;
   }
-)()
+  p.stop();
+}
