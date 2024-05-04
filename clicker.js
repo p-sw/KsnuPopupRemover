58b3f980468949a438ed13eead12db6e941cf65c
Shinwoo PARK <devpysweb@gmail.com>
2024-05-04 20:24:01 +0900

    fix: find iframe and click it

--- clicker.js
+++ clicker.js
@@ -1,9 +1,16 @@
 alert("팝업 제거 확장 프로그램이 실행되었습니다.")
 setInterval(function() {
-	var lecture_btn = document.querySelector('body > form > div > div.footer > ul > li > a');
-	console.log("lecture_btn:", lecture_btn);
-	if (!!lecture_btn && !!lecture_btn.click) {
-		lecture_btn.click();
-		console.log("클릭됨!");
-	}
+	let lecture_iframe = document.getElementById("contentsCheckForm");
+
+	console.log("lecture_iframe:", lecture_iframe);
+	if (!lecture_iframe)
+		return;
+
+	let lecture_btn = lecture_iframe.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
+	console.log("lecture_btn: ", lecture_btn);
+
+	if (!lecture_btn || typeof lecture_btn.click !== "function")
+		return;
+	
+	lecture_btn.click();
 }, 500);
