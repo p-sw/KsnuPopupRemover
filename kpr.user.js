175991b6658a16fc5c9a6efc1c452163d8297fe0
Shinwoo PARK <devpysweb@gmail.com>
2024-09-02 10:51:07 +0900

    Update kpr.user.js

--- kpr.user.js
+++ kpr.user.js
@@ -13,20 +13,20 @@
   'use strict';
 
   class R /* PopupRemover */ {
-		constructor() {}
-		t() {
-			this.i /* interval */ = setInterval(function() {
-				let i /* iframe */ = document.getElementById("contentsCheckForm");
-				console.log("lecture_iframe:", i);
-				if (!i) return
-				let b /* button */ = i.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
-				console.log("lecture_btn: ", b);
-				if (!b || typeof b.click !== "function") return;
-				b.click();
-			}, 500);
-		}
+    constructor() {}
+    t() {
+      this.i /* interval */ = setInterval(function() {
+        let i /* iframe */ = document.getElementById("contentsCheckForm");
+        console.log("lecture_iframe:", i);
+        if (!i) return
+        let b /* button */ = i.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
+        console.log("lecture_btn: ", b);
+        if (!b || typeof b.click !== "function") return;
+        b.click();
+      }, 500);
+    }
 
-		p() {
+    p() {
       if (this.i) clearInterval(this.i);
       this.i = null;
     }
