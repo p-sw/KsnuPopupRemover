a928e714dc6840d6951bfbe86903fd5a1f95d4ae
Shinwoo PARK <devpysweb@gmail.com>
2024-09-03 18:01:55 +0900

    Update loaderscript.js

--- loaderscript.js
+++ loaderscript.js
@@ -1,30 +1,30 @@
 class R /* PopupRemover */ {
-	constructor() {}
+  constructor() {}
 	
-	start() {
-		this.i /* interval */ = setInterval(function() {
-			let i /* iframe */ = document.getElementById("contentsCheckForm");
+  start() {
+    this.i /* interval */ = setInterval(function() {
+      let i /* iframe */ = document.getElementById("contentsCheckForm");
 		
-			console.log("lecture_iframe:", i);
-			if (!i)
-				return;
+      console.log("lecture_iframe:", i);
+      if (!i)
+        return;
 		
-			let b /* button */ = i.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
-			console.log("lecture_btn: ", b);
+      let b /* button */ = i.contentWindow.document.querySelector('body > form > div > div.footer > ul > li > a');
+      console.log("lecture_btn: ", b);
 		
-			if (!b || typeof b.click !== "function")
-				return;
+      if (!b || typeof b.click !== "function")
+        return;
 			
-			b.click();
-		}, 500);
-	}
+      b.click();
+    }, 500);
+  }
 
-	stop() {
-		if (this.i) {
-			clearInterval(this.i);
-			this.i = null;
-		}
-	}
+  stop() {
+    if (this.i) {
+      clearInterval(this.i);
+      this.i = null;
+    }
+  }
 }
 
 p = new R();
